d7c383412942a1e14047ac17c05729b3c333285a
benzene190 <34062138+benzene190@users.noreply.github.com>
2019-02-15 18:40:38 +0530

    Add files via upload
    Third Commit

--- listOfEmp.java
+++ listOfEmp.java
@@ -6,7 +6,9 @@ class Node{
 	String name;
 	int level;
 	Node parent;
-	LinkedList<Node> children = new LinkedList<Node>();
+	Node next=null;
+	Node prev=null;
+	lList children = new lList();
 	
 	Node(String a, Node p){
 		this.name=a;
@@ -21,10 +23,49 @@ class BSTNode{
 	BSTNode rchild=null;
 	Node ref;
 }
+
+class lList{
+	Node head;
+	Node tail;
+	
+	void add(Node X) {
+		if(head==null && tail==null) {
+			head = X;
+			tail = X;
+		}
+		else {
+			tail.next = X;
+			X.prev = tail;
+			tail = X;
+		}
+	}
+	
+	void remove(Node X) {
+		if(head==X && tail==X) {
+			head=null;
+			tail=null;
+		}
+		else if(tail==X) {
+			X.prev.next=null;
+			tail = X.prev;
+		}
+		else if(head==X) {
+			X.next.prev = null;
+			head = X.next;
+		}
+		else {
+			X.prev.next = X.next;
+			X.next.prev = X.prev;
+		}
+		
+	}
+}
+
 public class listOfEmp {
 	
 	static BSTNode BSTroot;
 	
+	static Node root;
 	
 	static void BSTInsert(String y, Node r) {
 		BSTNode t = BSTroot;
@@ -52,6 +93,51 @@ public class listOfEmp {
 		}
 	}
 	
+	static void BSTDelete(String y) {
+		BSTNode t = BSTroot;
+		int l=-1;
+		while(t.name!=y && t!=null) {
+			if(y.compareTo(t.name)<0) {
+				t=t.lchild;
+				l=1;
+			}
+			else if(y.compareTo(t.name)>0) {
+				t=t.rchild;
+				l=0;
+			}
+		}
+		
+		if(t.lchild==null && t.rchild==null) {
+			if(l==1) {
+				t.parent.lchild=null;
+			}
+			else if(l==0) {
+				t.parent.rchild=null;
+			}
+		}
+		else if(t.lchild==null) {
+			t=t.rchild;
+			t.rchild=null;
+		}
+		else if(t.rchild==null ) {
+			t=t.lchild;
+			t.lchild=null;
+		}
+		else {
+			BSTNode min=t.rchild;
+			while(min.lchild!=null) {
+				min=min.lchild;
+			}
+			t.name=min.name;
+			t.lchild=min.lchild;
+			t.rchild=min.rchild;
+			t.parent=min.parent;
+			t.ref=min.ref;
+			min.parent.lchild=null;
+			
+		}
+	}
+	
 	static Node Search(String x) {
 		BSTNode t = BSTroot;
 		while(t.name!=x && t!=null) {
@@ -66,6 +152,58 @@ public class listOfEmp {
 	}
 	
 	static void AddEmployee(String S, String Sp) throws NullPointerException{
+		Node searched = Search(S);
+		
+		if(searched==null) {
+			throw new NullPointerException("The parent doesn't exist");
+		}
+		else {
+			Node e = new Node(Sp, searched);
+			searched.children.add(e);
+			e.level = searched.level + 1;
+			BSTInsert(Sp,e);
+		}
+	}
+	
+	static void DeleteEmployee(String S, String Sp) throws NullPointerException{
+		Node searched1 = Search(S);
+		Node searched2 = Search(Sp);
+		
+		if(searched1==null || searched2==null) {
+			throw new NullPointerException("The specified node does not exist");
+		}
+		else {
+			if(searched2.children.tail==null && searched2.children.head==null) {
+				searched2.children.head=searched1.children.head;
+				searched2.children.tail=searched1.children.tail;
+			}
+			else {
+				searched2.children.tail.next = searched1.children.head;
+				searched1.children.head.prev = searched2.children.tail;
+				searched2.children.tail = searched1.children.tail;
+			}
+			searched1.parent.children.remove(searched1);
+			BSTDelete(S);
+		}
+	}
+	
+	static void PrintEmployees() {
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+		while(!q.isEmpty()) {
+			Node t = q.poll();
+			System.out.println(t.name + " ");
+			Node x = t.children.head;
+			while(x!=null) {
+				q.add(x);
+				x=x.next;
+			}
+		}
+	}
+	
+	static void LowestCommonBoss(String S, String Sp) {
+		Node searched1 = Search(S);
+		Node searched2 = Search(Sp);
 		
 	}
 	
@@ -83,11 +221,13 @@ public class listOfEmp {
 	  
 	  Node q = new Node(s2,null);
 	  Node p = new Node(s1,null);
+	  q.level = 0;
 	  q.children.add(p);
 	  p.parent = q;
 	  
 	  BSTroot.name = s2;
 	  BSTroot.ref = q;
+	  root=q;
 	  BSTroot.parent = null;
 	  BSTInsert(s1, p);
 	  
@@ -105,7 +245,7 @@ public class listOfEmp {
 		  
 		  else {
 			  searched.children.add(r);
-			  r.parent=searched;
+			  r.parent = searched;
 			  r.level = searched.level + 1;
 			  BSTInsert(s1, r);
 		};
